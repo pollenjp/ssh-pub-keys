a15427d9b7d14a458e87f89ee538718dc22c5391
pollenjp <polleninjp@gmail.com>
2022-09-10 10:11:12 +0900

    Update

--- fetch-ssh-keys.sh
+++ fetch-ssh-keys.sh
@@ -2,9 +2,15 @@
 set -eu
 
 keys_filepath="${HOME}/.ssh/fetched-keys.txt"
+merged_keys_filepath="${HOME}/.ssh/merged-keys.txt"
 authorized_keys_filepath="${HOME}/.ssh/authorized_keys"
+
+touch "${keys_filepath}" && chmod 600 "${keys_filepath}"
+touch "${merged_keys_filepath}" && chmod 600 "${merged_keys_filepath}"
+touch "${authorized_keys_filepath}" && chmod 600 "${authorized_keys_filepath}"
 
 wget "https://pollenjp.github.io/ssh-pub-keys/keys.txt" \
     -O "${keys_filepath}"
 
-authorized-keys-merger "${authorized_keys_filepath}" "${keys_filepath}"
+authorized-keys-merger "${authorized_keys_filepath}" "${keys_filepath}" > "${merged_keys_filepath}"
+cat "${merged_keys_filepath}" > "${authorized_keys_filepath}"
--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ cargo install authorized-keys-merger
 fetch_script_name="fetch-ssh-keys.sh"
 target_script_path="${HOME}/.ssh/${fetch_script_name}"
 
-cp "${fetch_script_name}" "${target_script_path}"
+cp "./${fetch_script_name}" "${target_script_path}"
 chmod 744 "${target_script_path}"
 
 if crontab -l 2>/dev/null | grep -q "${fetch_script_name}"; then
