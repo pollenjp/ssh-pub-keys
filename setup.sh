3e82cc6725f20ea6dd7ab2237d384446945e5db9
pollenjp <polleninjp@gmail.com>
2022-09-10 09:25:39 +0900

    Fix order

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ cargo install authorized-keys-merger
 
 fetch_script_name="fetch-ssh-keys.sh"
 target_script_path="${HOME}/.ssh/${fetch_script_name}"
-chmod 744 "${target_script_path}"
 
 cp "${fetch_script_name}" "${target_script_path}"
+chmod 744 "${target_script_path}"
 
 if crontab -l 2>/dev/null | grep -q "${fetch_script_name}"; then
     echo "Cron job already exists"
